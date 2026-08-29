c48925e361cebbe11b946533eeef617dbe200f8a
Bruno Martins <bgcngm@gmail.com>
2026-08-29 23:53:05 +0100

    odroidc4: Populate vendor with firmware
    These files were imported from the device tree.

---
+++ Android.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),odroidc4)
+
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,4842a6a654a53d672d45877bc2d6e183a448dedb)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,747c1252aafdc27b6437ae5184da78030a887b1c)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,c2d6cf7d98ce6e1f395fead67d6ed2b24189a6a9)
+$(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
+
+endif
