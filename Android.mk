64a2483f1d8a410d29060c0d4d046bb05795c13a
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-02 11:55:22 -0400

    odroidc4: Update bootloader image prebuilts
    * Old toolchains are no longer available without sign-in,
      update to use public ones.

--- Android.mk
+++ Android.mk
@@ -6,9 +6,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),odroidc4)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,4842a6a654a53d672d45877bc2d6e183a448dedb)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,747c1252aafdc27b6437ae5184da78030a887b1c)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,c2d6cf7d98ce6e1f395fead67d6ed2b24189a6a9)
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,51d30c156a99b9ae214b1faaa8b92dbc476974a9)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,92d523b9ad878bbc9d609820a7273e56ce9e7e1f)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,8e4407a4a843384f306aea16509c10a960bbb94c)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
