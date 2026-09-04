12958c9127816bc1f50b9acd973f530bbc69b94b
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-04 16:20:25 -0400

    odroidc4: Update bootloader image prebuilts

--- Android.mk
+++ Android.mk
@@ -6,9 +6,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter odroidc4 odroidc4_tab,$(TARGET_DEVICE)),)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,865aec3cbf86089c7d5f93b2fae0bf95b31e03d8)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,7571b58151aa6f39ce2b6acc504ec3cc1d0a52e1)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,474eebe6de991d45b168707a496a84309c92da03)
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,14eace9c7a7d15c614dd43253958312562afdbfa)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,682c50814d2875524d5eb783b62bb17dd532c34b)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,53b4b340fe505c1013e776feaa5564b11a8c45b3)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
